18011c614d5809397dfff01cd5ae7df3c3361fca
marlenvillela <mard_villelap52@unicah.edu>
2025-12-18 22:40:30 -0600

    Actualizacion de PublicController

---
+++ Controllers/PublicController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Controllers;
+
+
+abstract class PublicController
+{
+    public function __construct()
+    {
+   
+    }
+
+
+    protected function isPostBack(): bool
+    {
+        return $_SERVER["REQUEST_METHOD"] === "POST";
+    }
+}
+
+
+
